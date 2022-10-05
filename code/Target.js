904badc9eccdf809e0acbc1765928e4f53aff6fb
MengGo <81141742@qq.com>
2022-10-04 23:19:28 -0300

    code for Target items page

--- code/Target.js
+++ code/Target.js
@@ -1,4 +1,4 @@
-//Product Page
+//Product Page: https://www.target.com/p/
 //get total number
 var unitPart = document.getElementsByClassName('h-padding-h-default');
 var unitText = unitPart[0].children[2].innerText;
@@ -19,4 +19,34 @@ unitPrice.innerHTML = "(" + moneyUnit + perPrice + " / " + unit + ")";
 unitPrice.style = "color:red";
 pricePart[0].appendChild(unitPrice);
 
-//Items Page
+//Items Page: https://www.target.com/c/     https://www.target.com/s
+//code for search page
+var allItems = document.getElementsByClassName('styles__StyledRow-sc-wmoju4-0 kEpCtb')[3];
+//allItems.children[i]: i from 0-25, exclude 6 and 16
+for (let i=0; i<26; i++){
+    if (i == 6 || i == 16){
+        continue;
+    }
+
+    var curItem = allItems.children[i];
+    var unitPart = curItem.children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0];
+    var unitText = unitPart.innerText;
+    //var numUnit = unitText.match(/.*-\s(\d*\.*\d*[a-z]*\s*[a-z]*)\s*-*.*/)[1]; //'52 fl oz'
+    var numUnit = unitText.match(/\S*\s*-\s(\d.*)\s*-*.*/)[1];
+    var num = numUnit.match(/(\d*\.*\d*).*/)[1]; //'52'
+    var unit = numUnit.match(/\d*\.*\d*\s*([A-Za-z]*\s*[A-Za-z]*).*/)[1]; //'fl oz'
+
+    var pricePart = curItem.children[0].children[0].children[0].children[1].children[0].children[0].children[1].children[0].children[0];
+    var price = pricePart.innerText; //'$3.99'
+    var moneyUnit = price[0]; //"$"
+
+    var res = parseFloat(price.slice(1,-1)) / parseFloat(num); //price.slice(1,-1) = '3.99'
+    var perPrice = res.toFixed(3); 
+    var unitPrice = document.createElement('span');
+    unitPrice.innerHTML = "(" + moneyUnit + perPrice + " / " + unit + ")";
+    unitPrice.style = "color:red";
+    pricePart.appendChild(unitPrice);
+    console.log(i);
+
+}
+
